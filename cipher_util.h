260ec0010f16b7e7c904a09cbc9053bfb2e8aa78
Alexander <88991975+ElongatedTusk@users.noreply.github.com>
2023-03-13 12:10:10 +0800

    Update cipher_util.h
    Add vigenere_cipher function

--- cipher_util.h
+++ cipher_util.h
@@ -1,7 +1,8 @@
 #ifndef CIPHER_UTIL_H
 #define CIPHER_UTIL_H
 
-void caesar_cipher(char* text, int shift, int is_encrypt);
-void brute_force_decrypt(char* text);
+void caesar_cipher(char *text, int shift, int is_encrypt);
+void brute_force_decrypt(char *text);
+void vigenere_cipher(char *text, char *key, int is_encrypt);
 
 #endif
