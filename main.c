32b9eee04792bc506bf22bf309d866c816fb9c85
Alexander <88991975+ElongatedTusk@users.noreply.github.com>
2023-03-13 12:08:42 +0800

    Update main.c
    Add vigenere to interface

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "cipher_util.h"
 
 int main() {
-    char text[100];
+    char text[100], key[100];
     int shift, choice;
 
     printf("Select an option:\n");
@@ -13,19 +14,38 @@ int main() {
 
     scanf("%d", &choice);
 
-    if(choice == 2) {
-        printf("Would you like to bruteforce or specify the number of spaces to shift?\n");
-        printf("1. Bruteforce\n");
-        printf("2. Specify\n");
+    printf("Select a cipher:\n");
+    printf("1. Caesar cipher\n");
+    printf("2. Vigenere cipher\n");
 
-        scanf("%d", &choice);
+    scanf("%d", &shift);
 
-        if(choice == 1) {
-            printf("Enter the plain-text string: ");
-            scanf(" %[^\n]s", text);
+    if(shift == 1) {  // Caesar cipher
+        if(choice == 2) {
+            printf("Would you like to bruteforce or specify the number of spaces to shift?\n");
+            printf("1. Bruteforce\n");
+            printf("2. Specify\n");
 
-            brute_force_decrypt(text);
-        } else if(choice == 2) {
+            scanf("%d", &choice);
+
+            if(choice == 1) {
+                printf("Enter the plain-text string: ");
+                scanf(" %[^\n]s", text);
+
+                brute_force_decrypt(text);
+            } else if(choice == 2) {
+                printf("Enter the number of spaces to shift: ");
+                scanf("%d", &shift);
+
+                printf("Enter the plain-text string: ");
+                scanf(" %[^\n]s", text);
+
+                caesar_cipher(text, shift, choice == 1 ? 1 : 0);
+            } else {
+                printf("Invalid option selected!\n");
+                return 0;
+            }
+        } else if(choice == 1) {
             printf("Enter the number of spaces to shift: ");
             scanf("%d", &shift);
 
@@ -35,16 +55,27 @@ int main() {
             caesar_cipher(text, shift, choice == 1 ? 1 : 0);
         } else {
             printf("Invalid option selected!\n");
-            return 0;
         }
-    } else if(choice == 1) {
-        printf("Enter the number of spaces to shift: ");
-        scanf("%d", &shift);
+    } else if(shift == 2) {  // Vigenere cipher
+        if(choice == 1) {
+            printf("Enter the Vigenere key: ");
+            scanf("%s", key);
 
-        printf("Enter the plain-text string: ");
-        scanf(" %[^\n]s", text);
+            printf("Enter the plain-text string: ");
+            scanf(" %[^\n]s", text);
 
-        caesar_cipher(text, shift, choice == 1 ? 1 : 0);
+            vigenere_cipher(text, key, 1);
+        } else if(choice == 2) {
+            printf("Enter the Vigenere key: ");
+            scanf("%s", key);
+
+            printf("Enter the cipher-text string: ");
+            scanf(" %[^\n]s", text);
+
+            vigenere_cipher(text, key, 0);
+        } else {
+            printf("Invalid option selected!\n");
+        }
     } else {
         printf("Invalid option selected!\n");
     }
